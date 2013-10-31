7b4865684785dc6e890a352950fbcee995a5d14d
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-31 17:01:26 +0000

    corrected new version to 1.4.2
    git-svn-id: https://plugins.svn.wordpress.org/zedity/tags/1.4.2@796726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zedity.php
+++ zedity.php
@@ -3,7 +3,7 @@
 Plugin Name: Zedity
 Plugin URI: http://zedity.com/plugin/wp
 Description: Editing Reinvented. Take your site to the next level and wow your audience.
-Version: 1.4.1
+Version: 1.4.2
 Author: Zuyoy LLC
 Author URI: http://zuyoy.com
 License: GPL3
