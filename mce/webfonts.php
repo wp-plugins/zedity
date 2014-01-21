9acf225fce4e537955630f1c100f87e221394643
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-21 23:41:42 +0000

    released version 2.1.2
    git-svn-id: https://plugins.svn.wordpress.org/zedity/tags/2.1.2@842764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mce/webfonts.php
+++ mce/webfonts.php
@@ -1,10 +1,13 @@
 <?php
 header('Content-type: text/css');
 @session_start();
-if (!isset($_SESSION['zedity_webfonts'])) die;
-foreach ($_SESSION['zedity_webfonts'] as $font) {
-	$fontname = explode(',',$font);
-	$fontname = urlencode($fontname[0]);
-	echo "@import url(\"//fonts.googleapis.com/css?family=$fontname\");\n";
+if (isset($_SESSION['zedity_webfonts'])) {
+	foreach ($_SESSION['zedity_webfonts'] as $font) {
+		$fontname = explode(',',$font);
+		$fontname = urlencode($fontname[0]);
+		echo "@import url(\"//fonts.googleapis.com/css?family=$fontname\");\n";
+	}
 }
-
+if (isset($_SESSION['zedity_customfontscss'])) {
+	echo $_SESSION['zedity_customfontscss'];
+}
