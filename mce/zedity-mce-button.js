aa11afc5cfe35fd63352d509b3aa34af854c8f18
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-19 03:34:54 +0000

    released version 3.1.2
    git-svn-id: https://plugins.svn.wordpress.org/zedity/tags/3.1.2@897856 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mce/zedity-mce-button.js
+++ mce/zedity-mce-button.js
@@ -4,6 +4,7 @@
 			var t = this;
 			t.url = url;
 			t.ed = ed;
+			t.bind = tinyMCE.dom.Event.bind ? 'bind' : 'add';
 
 			ed.addButton('zedity', {
 				title: 'Zedity Editor',
@@ -19,12 +20,13 @@
 			});
 
 			ed.onPreInit.add(function(){
-				//be sure iframes are allowed
-				ed.schema.addValidElements('iframe[*]');
+				//be sure iframes and styles are allowed
+				ed.schema.addValidElements('iframe[*],style[*]');
 				//set zedity wrapper size when in visual editor (to show full overlay)
 				ed.parser.addNodeFilter('div', function(nodes){
 					for (var i=nodes.length-1; i>=0; --i) {
 						if (!nodes[i].attributes.map.class || nodes[i].attributes.map.class.indexOf('zedity-iframe-wrapper')==-1) continue;
+						if (!nodes[i].attributes.map['data-origw'] || !nodes[i].attributes.map['data-origh']) continue;
 						nodes[i].attr({
 							style: 'width:'+nodes[i].attributes.map['data-origw']+'px;height:'+nodes[i].attributes.map['data-origh']+'px'
 						});
@@ -34,9 +36,20 @@
 				ed.serializer.addNodeFilter('div', function(nodes,name,args){
 					for (var i=nodes.length-1; i>=0; --i) {
 						if (!nodes[i].attributes.map.class || nodes[i].attributes.map.class.indexOf('zedity-iframe-wrapper')==-1) continue;
+						if (!nodes[i].attributes.map['data-origw'] || !nodes[i].attributes.map['data-origh']) continue;
 						nodes[i].attr({
 							'data-mce-style': 'max-width:'+nodes[i].attributes.map['data-origw']+'px;max-height:'+nodes[i].attributes.map['data-origh']+'px'
 						});
+					}
+				});
+				//restore onclick on boxes with link
+				ed.serializer.addNodeFilter('div', function(nodes,name,args){
+					for (var i=nodes.length-1; i>=0; --i) {
+						if (nodes[i].attributes.map['data-href']) {
+							nodes[i].attr({
+								onclick: "window.open('"+nodes[i].attributes.map['data-href']+"','"+(nodes[i].attributes.map['data-target']||'_self')+"');" 
+							});
+						}
 					}
 				});
 				//temporarily disabled iframe management
@@ -75,15 +88,14 @@
 			//manage overlay
 			ed.onInit.add(function(ed){
 				//show overlay on click over zedity content
-				ed.dom.events.add(ed.getBody(), 'mousedown', function(e){
+				ed.dom.events[t.bind](ed.getBody(), 'mousedown', function(e){
 					var parent = ed.dom.getParent(e.target,'div.zedity-editor') || ed.dom.getParent(e.target,'div.zedity-iframe-wrapper');
 					if (parent) {
 						try {
 							ed.execCommand('mceFocus',false);
 						} catch (e) {}
 						t._showOverlay(ed,parent);
-						ed.dom.events.prevent(e);
-						ed.dom.events.stop(e);
+						ed.dom.events.cancel(e);
 						ed.plugins.wordpress._hideButtons();
 					}
 				});
@@ -93,56 +105,54 @@
 					if (parent) {
 						t._showOverlay(ed,parent);
 					} else {
+						if (n.attributes.class && ((' '+n.attributes.class.nodeValue+' ')||'').indexOf(' zedity-wrapper ')>-1) return;
 						t._hideOverlay();
 					}
 				});
 				//block enter key when overlay is open
-				ed.dom.events.add(ed.getBody(), 'keydown', function(e){
+				ed.dom.events[t.bind](ed.getBody(), 'keydown', function(e){
 					if (e.keyCode==13) {
 						var n = tinymce.DOM.get('zedity_content_overlay');
 						if (n && n.style.display=='block') {
-							ed.dom.events.prevent(e);
-							ed.dom.events.stop(e);
+							ed.dom.events.cancel(e);
 						}
 					} else if (e.keyCode==46 || e.keyCode==8) {
 						if (ed.selection.isCollapsed()) {
 							var n = ed.selection.getNode();
 							if (n.getAttribute('class') && n.getAttribute('class').indexOf('zedity')>-1) {
-								ed.dom.events.prevent(e);
-								ed.dom.events.stop(e);
+								ed.dom.events.cancel(e);
 								return;
 							}
 							var nc = n.textContent;
 							var empty = (nc.replace(/^\s+|\s+$/g, '') === '');
 							if (empty) {
 								tinymce.DOM.remove(n);
-								ed.dom.events.prevent(e);
-								ed.dom.events.stop(e);
+								ed.dom.events.cancel(e);
 							}
 						}
 					}
 				});
-
+				
 				//hide overlay when editor loses focus
-				tinyMCE.dom.Event.add(ed.getWin(), 'blur', function(){
+				tinyMCE.dom.Event[t.bind](ed.getWin(), 'blur', function(){
 					t._hideOverlay();
 				});
 
 				//reposition/resize overlay on WP editor scroll
-				tinyMCE.dom.Event.add(ed.getWin(), 'scroll', function(){
+				tinyMCE.dom.Event[t.bind](ed.getWin(), 'scroll', function(){
 					t._showOverlay(ed,t._zedityContent);
 				});
 				//reposition/resize overlay on WP editor resize
-				tinyMCE.dom.Event.add(ed.getWin(), 'resize', function(){
+				tinyMCE.dom.Event[t.bind](ed.getWin(), 'resize', function(){
 					t._showOverlay(ed,t._zedityContent);
 				});
 
 				//reposition/resize overlay on window scroll
-				tinyMCE.dom.Event.add(ed.getWin().parent, 'scroll', function(){
+				tinyMCE.dom.Event[t.bind](ed.getWin().parent, 'scroll', function(){
 					t._showOverlay(ed,t._zedityContent);
 				});
 				//reposition/resize overlay on window resize
-				tinyMCE.dom.Event.add(ed.getWin().parent, 'resize', function(){
+				tinyMCE.dom.Event[t.bind](ed.getWin().parent, 'resize', function(){
 					t._showOverlay(ed,t._zedityContent);
 				});
 			});
@@ -310,7 +320,7 @@
 				title: ed.getLang('zedity.edit_content')
 			});
 
-			tinymce.dom.Event.add(zEditButton, 'mousedown', function(e){
+			tinymce.dom.Event[t.bind](zEditButton, 'mousedown', function(e){
 				ed.selection.select(t._zedityContent);
 				t._openZedity();
 			});
@@ -323,7 +333,7 @@
 				title: ed.getLang('zedity.delete_content')
 			});
 
-			tinymce.dom.Event.add(zDelButton, 'mousedown', function(e){
+			tinymce.dom.Event[t.bind](zDelButton, 'mousedown', function(e){
 				var n = t._zedityContent;
 				//if is the inner content, get the outer wrapper
 				if (tinymce.DOM.hasClass(n,'zedity-iframe-wrapper') || tinymce.DOM.hasClass(n,'zedity-editor')) {
@@ -347,7 +357,7 @@
 				author: 'Zuyoy LLC',
 				authorurl: 'http://zedity.com',
 				infourl: 'http://zedity.com',
-				version: '2.0'
+				version: '2.1'
 			};
 		}
 	});
