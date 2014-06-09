508d4c7940808fa097f65aa9e33186a87b06085a
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-09 21:42:03 +0000

    released version 3.6.1
    git-svn-id: https://plugins.svn.wordpress.org/zedity/trunk@929112 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zedity.php
+++ zedity.php
@@ -3,7 +3,7 @@
 Plugin Name: Zedity
 Plugin URI: http://zedity.com/plugin/wp
 Description: The Best Editor to create any design you want, very easily and with unprecedented possibilities!
-Version: 3.6.0
+Version: 3.6.1
 Author: Zuyoy LLC
 Author URI: http://zuyoy.com
 License: GPL3
@@ -47,7 +47,7 @@ if (class_exists('WP_Zedity_Plugin')) {
 		const DEFAULT_WIDTH = 600; // a typical width for some themes in wordpress
 		
 		const MIN_HEIGHT = 20; // pixels
-		const MAX_HEIGHT = 6500; // pixels
+		const MAX_HEIGHT = 6000; // pixels
 		const DEFAULT_HEIGHT = 600;
 		
 		const WARNING_CONTENT_SIZE = 1000000; // 1MB (system dependent, current value based on observed cases)
