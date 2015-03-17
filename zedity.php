893267cea1c9be282df469ba6920f109c513d5db
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-17 01:00:57 +0000

    released version 5.0.1
    git-svn-id: https://plugins.svn.wordpress.org/zedity/trunk@1114264 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zedity.php
+++ zedity.php
@@ -3,7 +3,7 @@
 Plugin Name: Zedity
 Plugin URI: http://zedity.com/plugin/wp
 Description: The Best Editor to create any design you want, very easily and with unprecedented possibilities!
-Version: 5.0.0
+Version: 5.0.1
 Author: Zuyoy LLC
 Author URI: http://zuyoy.com
 License: GPL3
