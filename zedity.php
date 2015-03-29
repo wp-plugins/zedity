1f2674f2b50ba9d96bce934d0617f3ee10420fed
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-29 14:42:25 +0000

    init setting
    git-svn-id: https://plugins.svn.wordpress.org/zedity/tags/5.0.1@1123096 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zedity.php
+++ zedity.php
@@ -42,7 +42,7 @@ if (class_exists('WP_Zedity_Plugin')) {
 
 	class WP_Zedity_Plugin {
 		
-		const MIN_WIDTH = 50; // pixels
+		const MIN_WIDTH = 51; // pixels
 		const MAX_WIDTH = 3000; // pixels
 		const DEFAULT_WIDTH = 600; // a typical width for some themes in wordpress
 		
