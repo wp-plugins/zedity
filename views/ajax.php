b9810dc386d242bf3b37ca8392903723a0fa4361
zuyoy <zuyoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 17:59:40 +0000

    Released major new version, 6.0.0
    git-svn-id: https://plugins.svn.wordpress.org/zedity/tags/6.0.0@1162850 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/ajax.php
+++ views/ajax.php
@@ -1,5 +1,8 @@
 <?php
-if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LENGTH']>0) {
+if (!is_user_logged_in()) {
+	//user is not logged in
+	$response = array('error' => __('Forbidden.','zedity'));
+} if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LENGTH']>0) {
 	$response = array(
 	    'error' => __('Post size may be too big','zedity') . " ({$_SERVER['CONTENT_LENGTH']})."
 	);
@@ -10,11 +13,17 @@ if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LEN
     switch ($_REQUEST['zaction']) {
 		//------------------------------------------------------------------------------------
 		case 'save':
-			if (empty($_POST['tk']) || !wp_verify_nonce($_POST['tk'],'zedity')) {
+			//check nonce token
+			if (empty($_POST['tk']) || empty($_POST['post_id']) || !wp_verify_nonce($_POST['tk'],"zedity-save-{$_POST['post_id']}")) {
 				$response = array(
 					'error' => __('Invalid request.','zedity'),
 					'reload' => 1
 				);
+				break;
+			}
+			//check user permission for post
+			if (!current_user_can('edit_post',$_POST['post_id'])) {
+				$response = array('error' => __('Forbidden.','zedity'));
 				break;
 			}
 			if (empty($_POST['content'])) {
@@ -34,6 +43,11 @@ if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LEN
 			$dir = wp_upload_dir();
 
 			if (!empty($_POST['id']) && $_POST['id']>0) {
+				//check user permission for attachment
+				if (!current_user_can('edit_post',$_POST['id'])) {
+					$response = array('error' => __('Forbidden.','zedity'));
+					break;
+				}
 				$attach_id = $_POST['id'];
 				//get file name
 				$oldfile = get_attached_file($attach_id);
@@ -58,7 +72,11 @@ if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LEN
 
 			$css = '<style>html,body{padding:0;margin:0}</style>';
 			$js = '';
-			if ($this->is_premium() && strpos($content,'zedity-responsive')!==FALSE) {
+			if ($this->is_premium() && strpos($content,'zedity-responsive-layout')!==FALSE) {
+				//responsive layout
+				$js = '<script type="text/javascript" src="' . plugins_url('zedity/zedity-responsive.min.js',dirname(__FILE__)) . '"></script>';
+			} else if ($this->is_premium() && strpos($content,'zedity-responsive')!==FALSE) {
+				//responsive scaling
 				$css .= '<style>.zedity-responsive{-webkit-transform-origin:0 0;-moz-transform-origin:0 0;-ms-transform-origin:0 0;-o-transform-origin:0 0;transform-origin:0 0}</style>';
 				$js = '<script type="text/javascript">(function(){var e=document.querySelector(\'.zedity-responsive\');if(!e)return;var ow=e.offsetWidth;var oh=e.offsetHeight;var ar=false;window.onresize=(function resize(){var w=window.innerWidth/ow;var h=window.innerHeight/oh;if (ar)w=h=Math.min(w,h);var y=e.style;y.webkitTransform=y.MozTransform=y.msTransform=y.OTransform=y.transform=\'scale(\'+w+\',\'+h+\')\';return resize;})();})();</script>';
 			}
@@ -122,12 +140,14 @@ if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LEN
 
 		//------------------------------------------------------------------------------------
 		case 'load':
-			if (empty($_REQUEST['tk']) || !wp_verify_nonce($_REQUEST['tk'],'zedity')) {
+			//check nonce token
+			if (empty($_REQUEST['tk']) || empty($_REQUEST['id']) || !wp_verify_nonce($_REQUEST['tk'],"zedity-load-{$_REQUEST['id']}")) {
 				$response = array('error' => __('Invalid request.','zedity'));
 				break;
 			}
-			if (empty($_REQUEST['id'])) {
-				$response = array('error' => __('Missing id parameter in request.','zedity'));
+			//check user permission for post
+			if (!current_user_can('edit_post',$_REQUEST['id'])) {
+				$response = array('error' => __('Forbidden.','zedity'));
 				break;
 			}
 
@@ -156,6 +176,102 @@ if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST) && $_SERVER['CONTENT_LEN
 			die;
 		break;
 
+		
+		//------------------------------------------------------------------------------------
+		case 'addcontent':
+			//check nonce token
+			if (empty($_POST['tk']) || !wp_verify_nonce($_POST['tk'], "zedity-addcontent-{$_POST['type']}".(($_POST['id']<0) ? '' : "-{$_POST['id']}"))) {
+				$response = array(
+					'error' => __('Invalid request.','zedity'),
+					'reload' => 1
+				);
+				break;
+			}
+			//check user permission for post
+			if ((($_POST['id']>=0) && !current_user_can('edit_post',$_POST['id'])) || (($_POST['id']<0) && !current_user_can('edit_posts'))) {
+				$response = array('error' => __('Forbidden.','zedity'));
+				break;
+			}
+			if (empty($_POST['content'])) {
+				$response = array('error' => __('The post content is empty.','zedity'));
+				break;
+			}
+			if (empty($_POST['id'])) {
+				$response = array('error' => sprintf(__('Missing %s parameter in request.','zedity'),'id'));
+				break;
+			}
+			if (!$this->is_premium() && (($_POST['id']<0) || ($_POST['type']=='page'))) {
+				$response = array('error' => __('Invalid request.','zedity'));
+				break;
+			}
+
+			$content = "<p>&nbsp;</p>" . stripslashes($_POST['content']) . "<p>&nbsp;</p>";
+			if ($_POST['id']<0) {
+				if (empty($_POST['type'])) {
+					$response = array('error' => sprintf(__('Missing %s parameter in request.','zedity'),'type'));
+					break;
+				}
+				global $user_ID;
+				//create new post
+				$post_id = wp_insert_post(array(
+					'post_content' => $content,
+					'post_status' => 'draft',
+					'post_author' => $user_ID,
+					'post_type' => $_POST['type'],
+				));
+				if ($post_id===0) {
+					$response = array('error' => __('Could not save post/page content.','zedity'));
+					break;
+				}
+				$response = array('id' => $post_id);
+			} else {
+				//get post
+				$post = get_post($_POST['id']);
+				//add content
+				if (!empty($_POST['position']) && $_POST['position']=='above') {
+					$post->post_content = $content . $post->post_content;
+				} else {
+					$post->post_content .= $content;
+				}
+				//update post
+				$ret = wp_update_post($post);
+				if ($ret===0) {
+					$response = array('error' => __('Could not save post/page content.','zedity'));
+					break;
+				}
+				$response = array('id' => $_POST['id']);
+			}
+		break;
+		
+		//------------------------------------------------------------------------------------
+		case 'closeadminnotice':
+			if (empty($_POST['tk']) || !wp_verify_nonce($_POST['tk'],'zedity-closeadminnotice')) {
+				$response = array('error' => __('Invalid request.','zedity'));
+				break;
+			}
+			if (empty($_POST['dismiss'])) {
+				$response = array('error' => sprintf(__('Missing %s parameter in request.','zedity'),'dismiss'));
+				break;
+			}
+			
+			//set transient for the specific message to not show it again:
+			//for 12 hours if it is "remind later" for 60 days if it is "close"
+			set_transient("zedity_an_dismiss_{$_POST['dismiss']}",'1', $_POST['type']=='close' ? 60*DAY_IN_SECONDS : 12*HOUR_IN_SECONDS);
+			
+			//clear message if it was already set
+			$notices = get_option('zedity_admin_notices', array());
+			if (is_array($notices)) {
+				foreach ($notices as $key => $notice) {
+					if ($notice[2]==$_POST['dismiss']) unset($notices[$key]);
+				}
+			}
+			update_option('zedity_admin_notices', $notices);
+			
+			$response = array();
+			
+		break;
+		
+		
 		//------------------------------------------------------------------------------------
 		default:
 			$response = array(
